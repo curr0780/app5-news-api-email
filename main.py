71e0137b84bf514cc4b39f8e13c946b56499705a
curr0780 <curr0780@gmail.com>
2025-06-02 17:58:21 -0700

    Day 27: access news titles and descriptions from newsapi.org

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = "8e19eef1243f4f0fbd5367901ce0eb37"
+url = "https://newsapi.org/v2/everything?q=tesla&from=" \
+    "2025-05-03&sortBy=publishedAt&apiKey=" \
+    "8e19eef1243f4f0fbd5367901ce0eb37"
+
+request = requests.get(url)
+content = request.json()
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
