4cb4318da55b56f9b1c201d79517fc440d00e0a3
curr0780 <curr0780@gmail.com>
2025-06-09 14:39:59 -0700

    Day 28: added new script that calls NASA astronomy API

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import requests
+import smtplib, ssl
 
-api_key = "" # get from email
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-    "2025-05-03&sortBy=publishedAt&apiKey=" \
-    "8e19eef1243f4f0fbd5367901ce0eb37"
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "curr0780@gmail.com"
+    password = ""
+    receiver = "curr0780@gmail.com"
+    context = ssl.create_default_context()
+
+    with open('news-today.txt', 'w', encoding='utf-8') as file:
+        file.writelines(message)
+
+    # with smtplib.SMTP_SSL(host, port, context=context) as server:
+    #     server.login(username, password)
+    #     server.sendmail(username, receiver, message)
+
+
+topic = "Hawaii"
+api_key = "" #removed for push to git
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2025-05-06&"
+       "sortBy=publishedAt&apiKey=8e19eef1243f4f0fbd5367901ce0eb37")
 
 request = requests.get(url)
 content = request.json()
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+print(type(content))
+
+message_body = ""
+for article in content['articles'][:10]:
+    message_body = (message_body +
+                    article['title'] + "\n")
+# for index, article in enumerate(content["articles"][:10]):
+#     message_body = message_body + article['title'] + "\n" article['description'] + "\n" + article['url'] + 2*"\n"
+
+#message_body = message_body.encode("utf-8")
+print(message_body)
+# send_email(message_body)
 
 # Exercise/challenge: send these titles and articles to my email address
 # use code from the portfolio app
---
+++ nasa-api.py
@@ -0,0 +1,19 @@
+import requests
+import streamlit as st
+
+# make the api call
+nasa_api_key = "" # removed for push to git
+url = ("https://api.nasa.gov/planetary/apod?"
+       "api_key=cT7J98BRaDnfNHhmkvkkq2fqpietDAXut28UXvJt")
+request = requests.get(url)
+content = request.json()
+#print(content)
+for key in content:
+    print(f"{key}, {content[key]}")
+
+# set up website basics
+st.set_page_config(layout="wide")
+st.title("NASA Astronomy Image of the Day")
+st.title(content['title'])
+st.image(content['url'], use_column_width=True)
+st.write(content['explanation'])
